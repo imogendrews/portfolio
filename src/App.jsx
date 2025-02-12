9edb6498d9e0dfccdb48d22084ecee9ac66f36ae
Imogen Drews <imogen.drews@filmuniversitaet.de>
2025-02-12 18:00:04 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import { Home } from './pages/Home'; // Adjust the path if needed
+
+
+const App = () => {
+  return (
+    <div>
+    
+      <Home />
+    </div>
+  );
+};
+
+export default App;
